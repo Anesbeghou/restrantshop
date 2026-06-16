bfd3e2bf79c069550bc4f8c2c37c75f72a4753d5
Anesbeghou <abouanesmoufid@gmail.com>
2026-06-16 14:50:01 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,8 @@ BEGIN
     CREATE TABLE meals (
         id INT IDENTITY(1,1) PRIMARY KEY, -- البديل الصحيح لـ AUTO_INCREMENT
         name NVARCHAR(255) NOT NULL,       -- NVARCHAR لدعم اللغة العربية بشكل ممتاز
-        price DECIMAL(10, 2) NOT NULL,
+        cost DECIMAL(10, 2) NOT NULL DEFAULT 0.00, -- الحقل الجديد لتكلفة الوجبة على المطعم
+        price DECIMAL(10, 2) NOT NULL,    -- سعر البيع للزبون
         created_at DATETIME DEFAULT GETDATE() -- البديل الصحيح للوقت الحالي
     );
 END
